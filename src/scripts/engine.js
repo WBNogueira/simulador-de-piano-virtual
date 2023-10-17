caeafd90d12ff30b94b54ea200d014416cc9e7a7
Wagner Nogueira <wbnogueira3@gmail.com>
2023-10-17 11:29:00 -0300

    Mapeamento das teclas, implementando o controle de volume e ativação das teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,8 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
+let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -14,11 +17,24 @@ const playTune = (key) => {
 };
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key);
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
 });
 
+const handleVolume = (e) => {
+   audio.volume = e.target.value;
+};
+
+const showHideKeys = () => {
+    pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
